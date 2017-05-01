435623cca77020732b1d51e2cc862c4b235cc9b3
sanatauqir <tauqirs@oregonstate.edu>
2017-04-30 22:59:29 -0700

    Quiz 2 files

---
+++ projects/932789700/randomString.c
@@ -0,0 +1,4 @@
+For the inputChar() function I found the range that would be significant based on the testme() function to include upper and lower case letters and symbols regarding opening and closing statements. It was best to include uppercase and lowercase letters because the " " is significant to this function
+
+For the inputString() function I first generated a random number to be the time when the "reset" string should be created to get to the error condition. Looking at the testme function only lowercase letters seem significant. When the random1 variable is not equal to the second random generated number then a random string is created.
+
